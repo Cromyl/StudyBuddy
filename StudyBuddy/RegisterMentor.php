3fe4c9b4e6fa4885a51be57c2dd0272d4f636159
ShrutiBilolikar <96984960+ShrutiBilolikar@users.noreply.github.com>
2023-03-24 20:29:47 +0530

    Update RegisterMentor.php

---
+++ StudyBuddy/RegisterMentor.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    
+        <h1>Register New User</h1>    
+        <form action="" method="POST">
+            <label for="Username">Username:</label><br>
+            <input  type="text" name="Username" id="Username" required placeholder="Username"><br><br>
+            <label for="UserID">UserID:</label><br>
+            <input  type="text" name="UserID" id="UserID" required placeholder="UserID"><br><br>
+             <label for="password">Password:</label><br>
+            <input type="text" name="password" id="password" required placeholder="Password"><br><br>
+    
+            <input type="submit" name="submit" id="submit">
+        </form>
+    </div>
+</body>
+</html>
