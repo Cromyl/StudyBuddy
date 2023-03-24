c2eb04d2686d1031fa1da93dcd52dfb2a8e361f2
ShrutiBilolikar <96984960+ShrutiBilolikar@users.noreply.github.com>
2023-03-24 23:12:58 +0530

    Update UserLogin.php

--- StudyBuddy/UserLogin.php
+++ StudyBuddy/UserLogin.php
@@ -5,21 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
-    <div class="bgimg"></div>
-    <div class="bgtext">
-
-        <h1>Login</h1>    
-        <form action="" method="POST">
-            <label for="name">ID</label><br>
-            <input  type="text" name="ID" id="ID" required placeholder="ID"><br><br>
-            <label for="password">Password:</label><br>
-            <input type="text" name="password" id="password" required placeholder="Password"><br><br>
-    
-            <input type="submit" name="submit" id="submit">
+    <div style="margin-top:100px ">
+        <h1>Login</h1>   
+        <form action="connect" method="POST">
+            <div class="mb-3" style="margin-bottom: -2rem!important">
+                <label for="name"  class="form-label">User ID</label><br>
+                <input class="form-control" type="text" name="ID" id="ID" required placeholder="ID"><br><br>
+        
+            </div>
+            <div class="mb-3"style="margin-bottom: -2rem!important">
+        
+                <label for="password"class="form-label">Password</label><br>
+                <input class="form-control" type="text" name="password" id="password" required placeholder="Password"><br><br>
+                
+            </div>
+            
+              <input class="btn btn-primary" type="submit" name="submit" id="submit">
+              
         </form>
     </div>
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
+ 
 </body>
 </html>
