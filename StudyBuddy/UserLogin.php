803e60fc1e5345df93b3b124b8a896cd95b71953
ShrutiBilolikar <96984960+ShrutiBilolikar@users.noreply.github.com>
2023-03-24 20:31:25 +0530

    Update UserLogin.php

---
+++ StudyBuddy/UserLogin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="bgimg"></div>
+    <div class="bgtext">
+
+        <h1>Login</h1>    
+        <form action="" method="POST">
+            <label for="name">ID</label><br>
+            <input  type="text" name="ID" id="ID" required placeholder="ID"><br><br>
+            <label for="password">Password:</label><br>
+            <input type="text" name="password" id="password" required placeholder="Password"><br><br>
+    
+            <input type="submit" name="submit" id="submit">
+        </form>
+    </div>
+</body>
+</html>
