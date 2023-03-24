71d6cfe65ddd0eaf0d2eacaeed70efa6699eae18
ShrutiBilolikar <96984960+ShrutiBilolikar@users.noreply.github.com>
2023-03-24 20:32:48 +0530

    Update RegisterUser.php

---
+++ StudyBuddy/RegisterUser.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <h1>Register New User</h1>    
+        <form action="" method="POST">
+            <label for="name">Name:</label><br>
+            <input  type="text" name="name" id="name" required placeholder="Name"><br><br>
+            <label for="Roll_Number">Roll_Number:</label><br>
+            <input  type="text" name="Roll_Number" id="Roll_Number" required placeholder="Roll_Number"><br><br>
+            <label for="Semester">Semester:</label><br>
+            <input  type="text" name="Semester" id="Semester" required placeholder="Semester"><br><br>      
+            <label for="password">Password:</label><br>
+            <input type="text" name="password" id="password" required placeholder="Password"><br><br>
+    
+            <input type="submit" name="submit" id="submit">
+        </form>
+    </div>
+</body>
+</html>
