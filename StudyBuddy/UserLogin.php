a2eb7d70c365fd2eeb06c8a03c451fa0809a3198
Cromyl <92011735+Cromyl@users.noreply.github.com>
2023-03-25 12:33:54 +0530

    User Login php

--- StudyBuddy/UserLogin.php
+++ StudyBuddy/UserLogin.php
@@ -1,3 +1,8 @@
+<?php
+     $conn=mysqli_connect('sql12.freesqldatabase.com','sql12608210','fBXhWL98H4','sql12608210') or die("Connection failed" .mysqli_connect_error());
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,9 +15,42 @@
 
 </head>
 <body>
+<?php
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        $id=$_POST['ID'];
+        $password=$_POST['password'];
+       
+
+      $sql="SELECT count(*) FROM Student WHERE Rollno='$id' AND Password='$password'";
+      $sql2="SELECT * FROM Student WHERE Rollno='$id' AND password='$password'";
+        
+        $query=mysqli_query($conn,$sql);
+        $row=mysqli_fetch_array($query);
+
+        $query2=mysqli_query($conn,$sql2);
+        $row2=mysqli_fetch_array($query2);
+        //print_r($row);
+      
+
+        if($row[0]==1)
+        {
+        // Register $username, $password and redirect to file "login_success.php"
+            session_start();
+            $karo=print_r($row2[0],true);
+            $_SESSION["uid"] = $karo;
+            header("location:HomePage.php");
+        }
+        else {
+            echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>Incorrect Details !!</strong> Please check User-ID and Password.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>';
+        }
+    }
+?>
     <div style="margin-top:100px ">
         <h1>Login</h1>   
-        <form action="connect" method="POST">
+        <form action="UserLogin.php" method="POST">
             <div class="mb-3" style="margin-bottom: -2rem!important">
                 <label for="name"  class="form-label">User ID</label><br>
                 <input class="form-control" type="text" name="ID" id="ID" required placeholder="ID"><br><br>
@@ -21,7 +59,7 @@
             <div class="mb-3"style="margin-bottom: -2rem!important">
         
                 <label for="password"class="form-label">Password</label><br>
-                <input class="form-control" type="text" name="password" id="password" required placeholder="Password"><br><br>
+                <input class="form-control" type="password" name="password" id="password" required placeholder="Password"><br><br>
                 
             </div>
             
