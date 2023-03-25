f0841741f78cc80d7160672f39cbb64b046aadd3
radhikag970 <115557846+radhikag970@users.noreply.github.com>
2023-03-25 19:17:36 +0530

    Add files via upload

--- StudyBuddy/UploadMaterial.php
+++ StudyBuddy/UploadMaterial.php
@@ -7,22 +7,17 @@
     <title>Upload Material</title>
 </head>
 <body>
-    <form action="buildplan_entry.php" method="post">
-    <h3>Build new Plan</h3>
-    <label for="">Plan Name:</label><br>
-    <input type="text" id="planname" name="planname">
+    <form action="" method="post">
+    <h3>Enter Details</h3>
+    <label for="">Material Type:</label><br>
+    <input type="text" id="type" name="type">
     <br>
-    <label for="">Cost:</label><br>
-    <input type="number" id="cost" name="cost">
+    <label for="">Name:</label><br>
+    <input type="text" id="name" name="name">
     <br>
-    <label for="">Duration:</label><br>
-    <input type="number" id="duration" name="duration">
+    <label for="">Link:</label><br>
+    <input type="text" id="link" name="link">
     <br>
-    <label for="">Trip#1:</label><br>
-    <input type="number" id="trip1" name="trip1">
-    <br>
-    <label for="">Trip#2:</label><br>
-    <input type="number" id="trip2" name="trip2">
     <br>
     <input type="submit" name="submit1" id="submit1">
     </form>
