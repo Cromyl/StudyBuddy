6c53c76b9965f6cfeaca90d2c22f3e7dfa94d932
VirajJagtap001 <jviraj293@gmail.com>
2023-03-26 19:13:09 +0530

    RegisterMentor

--- StudyBuddy/RegisterMentor.php
+++ StudyBuddy/RegisterMentor.php
@@ -6,43 +6,18 @@
 <head>
     <script type="text/javascript">
     function validate() {
-        var id = document.forms["myform"]["UserID"].value;
-        if(id==""){
-            alert("Please enter your ID!");
-            return false;
-        }
         var name = document.forms["myform"]["name"].value;
-        if(name==""){
-            alert("Please enter your name!");
+        var reg=/[^a-zA-Z\ ]+/;
+        if (reg.test(name)){
+            alert("Name should only contain alphabets and spaces!");
             return false;
         }
-        else{
-            var reg=/[^a-zA-Z\ ]+/;
-            if (reg.test(name)){
-                alert("Name should only contain alphabets and spaces!");
-                return false;
-            }
-        }
-        var role = document.forms["myform"]["Type"].value;
-        if(role==""){
-            alert("Please Enter your Role!");
-            return false;
-        }
-        else if(role != "A" && role != "P" && role != "T"){
-            alert("Enter your Role (A/P/T)!");
-            return false;
-        }
+
         var password = document.forms["myform"]["password"].value;
-        if(password==""){
-            alert("Please enter the password");
+        var reg=/[^a-zA-Z0-9\!\@\#\$]+/;
+        if (reg.test(password)){
+            alert("Password should not contain characters other than A-Z/a-z/0-9/!/@/#/$");
             return false;
-        }
-        else{
-            var reg=/[^a-zA-Z0-9\!\@\#\$]+/;
-            if (reg.test(password)){
-                alert("Password should not contain characters other than A-Z/a-z/0-9/!/@/#/$");
-                return false;
-            }
         }
     }
 </script>
@@ -54,13 +29,13 @@
 <body>
 <?php
     if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['submit'])){
-        if(isset($_POST['name'])&& isset($_POST['Type']) && isset($_POST['UserID']) && isset($_POST['password'])){
-        $UserID=$_POST['UserID'];
-        $name=$_POST['name'];
-        $Type=$_POST['Type'];
-        $password=$_POST['password'];
-        //if(($Type=='P' || $Type=='A' || $Type=='T' )&& ctype_alpha($name)==true){
-            $sql="INSERT INTO Mentor VALUES ('$UserID','$name','$Type','$password')";
+        if(isset($_POST['name'])&& isset($_POST['type']) && isset($_POST['UserID']) && isset($_POST['password'])){
+            $UserID=$_POST['UserID'];
+            $name=$_POST['name'];
+            $type=$_POST['type'];
+            $password=$_POST['password'];
+            
+            $sql="INSERT INTO Mentor VALUES ('$UserID','$name','$type','$password')";
             $query =mysqli_query($conn,$sql);
             if($query){
                 
@@ -77,18 +52,22 @@
 //}
 ?>      
 <div>
-        <h1>Register New User</h1>    
-            <form name="myform" action="RegisterMentor.php" onsubmit="return validate()" method="POST">
-            <label for="UserID">UserID:</label><br>
-            <input  type="text" name="UserID" id="UserID" required placeholder="UserID"><br><br>
-            <label for="name">Name:</label><br>
-            <input  type="text" name="name" id="name" required placeholder="Username"><br><br>
-            <label for="Type">Role(P/T/A):</label><br>
-            <input  type="text" name="Type" id="Type" required placeholder="Role"><br><br>
-            <label for="password">Password:</label><br>
-            <input type="text" name="password" id="password" required placeholder="Password"><br><br>
-            <input type="submit" name="submit" id="submit">
-            </form>
+    <h1>Register New User</h1>    
+    <form name="myform" action="RegisterMentor.php" onsubmit="return validate()" method="POST">
+    <label for="UserID">UserID:</label><br>
+    <input  type="text" name="UserID" id="UserID" required placeholder="UserID"><br><br>
+    <label for="name">Name:</label><br>
+    <input  type="text" name="name" id="name" required placeholder="Username"><br><br>
+    <label for="type">Role:</label><br>
+    <select name="type" id="type">
+        <option value="A">Admin</option>
+        <option value="P">Professor</option>
+        <option value="T">Teaching Assistant</option>
+    </select><br><br>
+    <label for="password">Password:</label><br>
+    <input type="password" name="password" id="password" required placeholder="Password"><br><br>
+    <input type="submit" name="submit" id="submit">
+    </form>
 </div>
 </body>
 </html>
