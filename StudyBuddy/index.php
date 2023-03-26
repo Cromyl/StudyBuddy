705cddb687c08d0005e001a2b9ba16621b6be3a5
radhikag970 <115557846+radhikag970@users.noreply.github.com>
2023-03-26 20:12:14 +0530

    Add files via upload

--- StudyBuddy/index.php
+++ StudyBuddy/index.php
@@ -1,3 +1,15 @@
+<?php
+    $conn=mysqli_connect('sql12.freesqldatabase.com','sql12608210','fBXhWL98H4','sql12608210') or die("Connection failed" .mysqli_connect_error());
+?>
+<?php
+     session_start();
+     $temp=$_SESSION['uid'];
+     $sql="SELECT * FROM Student WHERE Rollno='$temp'";
+     $query=mysqli_query($conn,$sql);
+     $row=mysqli_fetch_array($query);
+     $rollno=$row['Rollno'];
+     $sem=$row['Semester'];
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,7 +17,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Business Frontpage - Start Bootstrap Template</title>
+        <title>Study Buddy</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
@@ -17,17 +29,16 @@
         <!-- Responsive navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container px-5">
-                <a class="navbar-brand" href="#!">Welcome!!</a>
+                <h3 class="navbar-brand">Welcome <?php echo $row['Name'];?>!!</h3>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">User Profile</a></li>
-                        <!-- <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Services</a></li> -->
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php"><?php echo "<h4>{$rollno}</h4>";?></a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php"><?php echo "<h4> Semester-{$sem}</h4>";?></a></li>
                     </ul>
                 </div>
             </div>
+            <a class="btn btn-primary btn-lg px-3 me-sm-3" href="UserLogin.php">Log Out</a>
         </nav>
         <!-- Header-->
         <header class="bg-dark py-5">
@@ -36,7 +47,7 @@
                     <div class="col-lg-6">
                         <div class="text-center my-5">
                             <h1 class="display-5 fw-bolder text-white mb-2">Study Buddy</h1>
-                            <p class="lead text-white-50 mb-4">Don't Just Learn, Experience!<br>Don't Just Read, Absorb!<br> Don't Just Think, Ponder! <br> Don't Just Dream, Do! <br> ~ Roy T. Bennett</p>
+                            <p class="lead text-white-50 mb-4">Don't Just Learn, Experience!<br>Don't Just Read, Absorb!<br> Don't Just Think, Ponder! <br> Don't Just Dream, Do! <br> - Roy T. Bennett</p>
                             <div class="d-grid gap-3 d-sm-flex justify-content-sm-center">
                                 <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Get Started</a>
                                 <!-- <a class="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a> -->
@@ -52,28 +63,28 @@
                 <div class="row gx-5">
                     <div class="col-lg-4 mb-5 mb-lg-0">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
-                        <h2 class="h4 fw-bolder">Featured title</h2>
-                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                        <a class="text-decoration-none" href="#!">
-                            Call to action
+                        <h2 class="h4 fw-bolder">Study Material</h2>
+                        <p>Get Subjectwise Notes, Assignments, Previous Year Question Papers and Reference Books at one place. Ofcourse Study Buddy makes it Easy!</p>
+                        <a class="text-decoration-none" href="Subject.php">
+                            View Study Material
                             <i class="bi bi-arrow-right"></i>
                         </a>
                     </div>
                     <div class="col-lg-4 mb-5 mb-lg-0">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
-                        <h2 class="h4 fw-bolder">Featured title</h2>
-                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                        <a class="text-decoration-none" href="#!">
-                            Call to action
+                        <h2 class="h4 fw-bolder">Discussion Page</h2>
+                        <p>Stuck Somewhere? Don't Worry Use our discussion feature to ask your doubt or have a look at previously asked questions.</p>
+                        <a class="text-decoration-none" href="DiscussionPage.php">
+                            Ask Doubt
                             <i class="bi bi-arrow-right"></i>
                         </a>
                     </div>
                     <div class="col-lg-4">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
-                        <h2 class="h4 fw-bolder">Featured title</h2>
-                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                        <a class="text-decoration-none" href="#!">
-                            Call to action
+                        <h2 class="h4 fw-bolder">Announcements</h2>
+                        <p>Check for new Updates regarding Assignment deadlines, upcoming exams,new doubts,their answers and much more.</p>
+                        <a class="text-decoration-none" href="Annoucement.php">
+                            View Announcements
                             <i class="bi bi-arrow-right"></i>
                         </a>
                     </div>
@@ -81,15 +92,15 @@
             </div>
         </section>
         <!-- Pricing section-->
-        <section class="bg-light py-5 border-bottom">
+        <!-- <section class="bg-light py-5 border-bottom">
             <div class="container px-5 my-5">
                 <div class="text-center mb-5">
                     <h2 class="fw-bolder">Pay as you grow</h2>
                     <p class="lead mb-0">With our no hassle pricing plans</p>
                 </div>
                 <div class="row gx-5 justify-content-center">
-                    <!-- Pricing card free-->
-                    <div class="col-lg-6 col-xl-4">
+                    Pricing card free-->
+                    <!-- <div class="col-lg-6 col-xl-4">
                         <div class="card mb-5 mb-xl-0">
                             <div class="card-body p-5">
                                 <div class="small text-uppercase fw-bold text-muted">Free</div>
@@ -134,9 +145,9 @@
                                 <div class="d-grid"><a class="btn btn-outline-primary" href="#!">Choose plan</a></div>
                             </div>
                         </div>
-                    </div>
+                    </div>  -->
                     <!-- Pricing card pro-->
-                    <div class="col-lg-6 col-xl-4">
+                    <!-- <div class="col-lg-6 col-xl-4">
                         <div class="card mb-5 mb-xl-0">
                             <div class="card-body p-5">
                                 <div class="small text-uppercase fw-bold">
@@ -185,8 +196,8 @@
                             </div>
                         </div>
                     </div>
-                    <!-- Pricing card enterprise-->
-                    <div class="col-lg-6 col-xl-4">
+                     Pricing card enterprise-->
+                    <!-- <div class="col-lg-6 col-xl-4">
                         <div class="card">
                             <div class="card-body p-5">
                                 <div class="small text-uppercase fw-bold text-muted">Enterprise</div>
@@ -235,7 +246,7 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section>  -->
         <!-- Testimonials section-->
         <section class="py-5 border-bottom">
             <div class="container px-5 my-5 px-5">
@@ -274,15 +285,15 @@
             </div>
         </section>
         <!-- Contact section-->
-        <section class="bg-light py-5">
+        <!-- <section class="bg-light py-5">
             <div class="container px-5 my-5 px-5">
                 <div class="text-center mb-5">
                     <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-envelope"></i></div>
                     <h2 class="fw-bolder">Get in touch</h2>
                     <p class="lead mb-0">We'd love to hear from you</p>
                 </div>
-                <div class="row gx-5 justify-content-center">
-                    <div class="col-lg-6">
+                <div class="row gx-5 justify-content-center"> -->
+                    <!-- <div class="col-lg-6"> -->
                         <!-- * * * * * * * * * * * * * * *-->
                         <!-- * * SB Forms Contact Form * *-->
                         <!-- * * * * * * * * * * * * * * *-->
@@ -290,59 +301,59 @@
                         <!-- To make this form functional, sign up at-->
                         <!-- https://startbootstrap.com/solution/contact-forms-->
                         <!-- to get an API token!-->
-                        <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                            <!-- Name input-->
-                            <div class="form-floating mb-3">
+                        <!-- <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                             Name input-->
+                            <!-- <div class="form-floating mb-3">
                                 <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
                                 <label for="name">Full name</label>
                                 <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
-                            </div>
+                            </div> -->
                             <!-- Email address input-->
-                            <div class="form-floating mb-3">
+                            <!-- <div class="form-floating mb-3">
                                 <input class="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
                                 <label for="email">Email address</label>
                                 <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
                                 <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-                            </div>
+                            </div> -->
                             <!-- Phone number input-->
-                            <div class="form-floating mb-3">
+                            <!-- <div class="form-floating mb-3">
                                 <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
                                 <label for="phone">Phone number</label>
                                 <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
-                            </div>
+                            </div> -->
                             <!-- Message input-->
-                            <div class="form-floating mb-3">
+                            <!-- <div class="form-floating mb-3">
                                 <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem" data-sb-validations="required"></textarea>
                                 <label for="message">Message</label>
                                 <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
-                            </div>
+                            </div> -->
                             <!-- Submit success message-->
                             <!---->
                             <!-- This is what your users will see when the form-->
                             <!-- has successfully submitted-->
-                            <div class="d-none" id="submitSuccessMessage">
+                            <!-- <div class="d-none" id="submitSuccessMessage">
                                 <div class="text-center mb-3">
                                     <div class="fw-bolder">Form submission successful!</div>
                                     To activate this form, sign up at
                                     <br />
                                     <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
                                 </div>
-                            </div>
+                            </div> -->
                             <!-- Submit error message-->
                             <!---->
                             <!-- This is what your users will see when there is-->
                             <!-- an error submitting the form-->
-                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                            <!-- <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div> -->
                             <!-- Submit Button-->
-                            <div class="d-grid"><button class="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
+                            <!-- <div class="d-grid"><button class="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
                         </form>
                     </div>
                 </div>
             </div>
-        </section>
+        </section>  -->
         <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container px-5"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+            <div class="container px-5"><p class="m-0 text-center text-white">Copyright &copy; studybuddy.com 2023</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
