df4db8899e284e3278638d72fac86b262729aef6
radhikag970 <115557846+radhikag970@users.noreply.github.com>
2023-03-26 16:00:50 +0530

    Add files via upload

--- StudyBuddy/RegisterMentor.php
+++ StudyBuddy/RegisterMentor.php
@@ -4,6 +4,48 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <script type="text/javascript">
+    function validate() {
+        var id = document.forms["myform"]["UserID"].value;
+        if(id==""){
+            alert("Please enter your ID!");
+            return false;
+        }
+        var name = document.forms["myform"]["name"].value;
+        if(name==""){
+            alert("Please enter your name!");
+            return false;
+        }
+        else{
+            var reg=/[^a-zA-Z\ ]+/;
+            if (reg.test(name)){
+                alert("Name should only contain alphabets and spaces!");
+                return false;
+            }
+        }
+        var role = document.forms["myform"]["Type"].value;
+        if(role==""){
+            alert("Please Enter your Role!");
+            return false;
+        }
+        else if(role != "A" && role != "P" && role != "T"){
+            alert("Enter your Role (A/P/T)!");
+            return false;
+        }
+        var password = document.forms["myform"]["password"].value;
+        if(password==""){
+            alert("Please enter the password");
+            return false;
+        }
+        else{
+            var reg=/[^a-zA-Z0-9\!\@\#\$]+/;
+            if (reg.test(password)){
+                alert("Password should not contain characters other than A-Z/a-z/0-9/!/@/#/$");
+                return false;
+            }
+        }
+    }
+</script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,7 +59,7 @@
         $name=$_POST['name'];
         $Type=$_POST['Type'];
         $password=$_POST['password'];
-        if(($Type=='P' || $Type=='A' || $Type=='T' )&& ctype_alpha($name)==true){
+        //if(($Type=='P' || $Type=='A' || $Type=='T' )&& ctype_alpha($name)==true){
             $sql="INSERT INTO Mentor VALUES ('$UserID','$name','$Type','$password')";
             $query =mysqli_query($conn,$sql);
             if($query){
@@ -28,15 +70,15 @@
                 echo 'Value exists';
             }
         }
-        else{
-            echo 'Invalid input';
-        }
+        // else{
+        //     echo 'Invalid input';
+        // }
     }
-}
+//}
 ?>      
 <div>
         <h1>Register New User</h1>    
-            <form action="RegisterMentor.php" method="POST">
+            <form name="myform" action="RegisterMentor.php" onsubmit="return validate()" method="POST">
             <label for="UserID">UserID:</label><br>
             <input  type="text" name="UserID" id="UserID" required placeholder="UserID"><br><br>
             <label for="name">Name:</label><br>
